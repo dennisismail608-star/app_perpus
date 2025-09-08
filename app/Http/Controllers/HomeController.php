1fb6cf1527a76017efbaf24bf7e92cf4d7f45aea
dennisismail608-star <dennisismail608>
2025-09-08 14:44:53 +0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\Borrows;
+use App\Models\DetailBorrows;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $totalBooks = Book::count();
+        $totalStock = Book::sum('stock');
+
+        // detail_buku ada yang tidak sedang dipinjam, actual_date = null
+
+        $borrowedBooks = DetailBorrows::with('book', 'borrow')->whereHas('borrow', function ($q) {
+            $q->whereNull('actual_return_date');
+        })->count();
+
+        $returnBooks = Borrows::where('status', 0)->whereNotNull('actual_return_date')->count();
+        $notReturnBooks = Borrows::where('status', 1)->whereNull('actual_return_date')->count();
+
+        $fines = Borrows::with('member')->where('fine', '>', 0)->get();
+        $totalFines = Borrows::sum('fine');
+        return view('admin.dashboard', compact('totalBooks', 'totalStock', 'borrowedBooks', 'returnBooks', 'notReturnBooks', 'fines', 'totalFines'));
     }
 }
